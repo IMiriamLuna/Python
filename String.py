eeeffa00a453ed0a276f365bdea61dec359a4a3c
MIRIAM ARACELI LUNA CARREON <129115493+IMiriamLuna@users.noreply.github.com>
2024-10-24 17:20:54 -0600

    Strings introduction

---
+++ String.py
@@ -0,0 +1,28 @@
+mensaje = "Hello"
+mensaje += " "
+mensaje += "World"
+
+print ("Mensaje: " + mensaje)
+
+mensaje = "Hello"
+espacio = " "
+nombre = "World"
+print("Concatenación: " + mensaje + espacio + nombre)
+
+numero_uno = 4
+numero_dos = 8
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+print("El resultado de la suma es: " + resultado)
+
+mensaje = "Hello World"
+buscar_subcadena = mensaje.find("World")
+print("Busqueda: " + str(buscar_subcadena))
+
+extraer_subcadena = mensaje[1:4]
+print("Extraccion: " + extraer_subcadena)
+
+mensaje_uno = "Hola"
+mensaje_dos = "World"
+print("Comparación: "+str(mensaje_uno==mensaje_dos))
+
